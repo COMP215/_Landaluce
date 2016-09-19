2a42dd06226913fc77bb86746481e75bce98e79f
alvaro <delandaluce_alvaro@wheatoncollege.edu>
2016-09-19 16:11:57 -0400

    add _ to class members

--- lab03/BinarySearchTree/main.cpp
+++ lab03/BinarySearchTree/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 class Node
 {
     public:
-        int data;
-        int count;
-        Node* left_child;
-        Node* right_child;
+        int data_;
+        int count_;
+        Node* left_child_;
+        Node* right_child_;
         Node();
         Node(int data);
         ~Node();
@@ -16,29 +16,29 @@ class Node
 
 Node::Node()
 {
-    this->count       = 1;
-    this->data        = NULL;
-    this->left_child  = NULL;
-    this->right_child = NULL;
+    this->count_       = 1;
+    this->data_        = NULL;
+    this->left_child_  = NULL;
+    this->right_child_ = NULL;
 }
 Node::Node(int data)
 {
-    this->count       = 1;
-    this->data        = data;
-    this->left_child  = NULL;
-    this->right_child = NULL;
+    this->count_       = 1;
+    this->data_        = data;
+    this->left_child_  = NULL;
+    this->right_child_ = NULL;
 }
 Node::~Node()
 {
-    delete(this->left_child);
-    delete(this->right_child);
+    delete(this->left_child_);
+    delete(this->right_child_);
 }
 
 class BinarySearchTree
 {
     public:
-        Node* head;
-        Node buffer;
+        Node* head_;
+        Node buffer_;
 
         BinarySearchTree();
         ~BinarySearchTree();
@@ -54,27 +54,27 @@ class BinarySearchTree
 
 BinarySearchTree::BinarySearchTree()
 {
-    this->head              = &this->buffer;
-    this->head->data        = NULL;
-    this->head->left_child  = NULL;
-    this->head->right_child = NULL;
+    this->head_               = &this->buffer_;
+    this->head_->data_        = NULL;
+    this->head_->left_child_  = NULL;
+    this->head_->right_child_ = NULL;
 }
 BinarySearchTree::~BinarySearchTree()
 {
-    DestroyTree(this->head);
+    DestroyTree(this->head_);
 }
 void BinarySearchTree::DestroyTree(Node* leaf)
 {
     if(leaf)
     {
-        DestroyTree(leaf->left_child);
-        DestroyTree(leaf->right_child);
+        DestroyTree(leaf->left_child_);
+        DestroyTree(leaf->right_child_);
         delete(leaf);
     }
 }
 void BinarySearchTree::Insert(int data)
 {
-    Node* p = this->head;
+    Node* p = this->head_;
 
     if(p)
     {
@@ -83,59 +83,59 @@ void BinarySearchTree::Insert(int data)
     else
     {
         p       = new Node;
-        p->data = data;
+        p->data_ = data;
     }
 }
 void BinarySearchTree::RecursiveInsert(int data, Node* leaf)
 {
-    if(data < leaf->data)
+    if(data < leaf->data_)
     {
-        if(leaf->left_child)
+        if(leaf->left_child_)
         {
-            RecursiveInsert(data, leaf->left_child);
+            RecursiveInsert(data, leaf->left_child_);
         }
         else
         {
-            leaf->left_child       = new Node;
-            leaf->left_child->data = data;
+            leaf->left_child_        = new Node;
+            leaf->left_child_->data_ = data;
         }
     }
-    else if(data > leaf->data)
+    else if(data > leaf->data_)
     {
-        if(leaf->right_child)
+        if(leaf->right_child_)
         {
-            RecursiveInsert(data,leaf->right_child);
+            RecursiveInsert(data,leaf->right_child_);
         }
         else
         {
-            leaf->right_child       = new Node;
-            leaf->right_child->data = data;
+            leaf->right_child_        = new Node;
+            leaf->right_child_->data_ = data;
         }
     }
     else
     {
-        leaf->count++;
+        leaf->count_++;
     }
 }
 Node* BinarySearchTree::Search(int data)
 {
-    return RecursiveSearch(data,this->head);
+    return RecursiveSearch(data,this->head_);
 }
 Node* BinarySearchTree::RecursiveSearch(int data, Node* node)
 {
     if(node)
     {
-        if(data == node->data)
+        if(data == node->data_)
         {
             return node;
         }
-        else if(data < node->data)
+        else if(data < node->data_)
         {
-            return RecursiveSearch(data,node->left_child);
+            return RecursiveSearch(data,node->left_child_);
         }
         else
         {
-            return RecursiveSearch(data,node->right_child);
+            return RecursiveSearch(data,node->right_child_);
         }
     }
     else
@@ -145,20 +145,20 @@ Node* BinarySearchTree::RecursiveSearch(int data, Node* node)
 }
 void BinarySearchTree::DisplayTree()
 {
-    RecursiveDisplay(this->head,0);
+    RecursiveDisplay(this->head_,0);
 }
 void BinarySearchTree::RecursiveDisplay(Node* node,int count)
 {
     if(node)
     {
-        if(node == this->head)
+        if(node == this->head_)
             cout << "head: ";
-        cout << node->data << "(" << node->count << ")" << endl;
+        cout << node->data_ << "(" << node->count_ << ")" << endl;
 
-        cout << endl << "[" << node->data << "]->left  child: ";
-        RecursiveDisplay(node->left_child,count);
-        cout << endl << "[" << node->data << "]->right child: ";
-        RecursiveDisplay(node->right_child,count);
+        cout << endl << "[" << node->data_ << "]->left  child: ";
+        RecursiveDisplay(node->left_child_,count);
+        cout << endl << "[" << node->data_ << "]->right child: ";
+        RecursiveDisplay(node->right_child_,count);
     }
     else
     {
@@ -172,25 +172,25 @@ void BinarySearchTree::DisplayNode(Node* node)
         Node* left_child  = new Node;
         Node* right_child = new Node;
 
-        left_child  = node->left_child;
-        right_child = node->right_child;
+        left_child  = node->left_child_;
+        right_child = node->right_child_;
 
-        cout << endl << "Node: " << node->data << "(" << node->count << ")" << endl;
+        cout << endl << "Node: " << node->data_ << "(" << node->count_ << ")" << endl;
         if(left_child)
         {
-            cout << "[" << node->data << "]->left   child: " << left_child->data << endl;
+            cout << "[" << node->data_ << "]->left   child: " << left_child->data_ << endl;
         }
         else
         {
-            cout << "[" << node->data << "]->left   child: " << "NULL" << endl;
+            cout << "[" << node->data_ << "]->left   child: " << "NULL" << endl;
         }
         if(right_child)
         {
-            cout << "[" << node->data << "]->right  child: " << right_child->data << endl;
+            cout << "[" << node->data_ << "]->right  child: " << right_child->data_ << endl;
         }
         else
         {
-            cout << "[" << node->data << "]->right  child: " << "NULL" << endl;
+            cout << "[" << node->data_ << "]->right  child: " << "NULL" << endl;
         }
 
     }
--- lab03/LinkedList/main.cpp
+++ lab03/LinkedList/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 class Node
 {
     public:
-        string data;
-        Node* next;
+        string data_;
+        Node* next_;
 
         Node();
         Node(string data);
@@ -14,25 +14,25 @@ class Node
 };
 Node::Node()
 {
-    this->data = "";
-    this->next = NULL;
+    this->data_ = "";
+    this->next_ = NULL;
 }
 Node::Node(string data)
 {
-    this->data = data;
-    this->next = NULL;
+    this->data_ = data;
+    this->next_ = NULL;
 }
 Node::~Node()
 {
-    delete(this->next);
+    delete(this->next_);
 }
 
 class LinkedList
 {
     public:
-        Node* head;
-        Node buffer;
-        Node* tail;
+        Node* head_;
+        Node  buffer_;
+        Node* tail_;
 
         LinkedList();
         void Push(string data);
@@ -45,33 +45,33 @@ class LinkedList
 };
 LinkedList::LinkedList()
 {
-    this->head = &buffer;
-    this->buffer.data = "Buffer";
-    this->tail = &buffer;
+    this->head_ = &buffer_;
+    this->buffer_.data_ = "Buffer";
+    this->tail_ = &buffer_;
 }
 void LinkedList::Push(string data)
 {
     Node* new_node = new Node(data);
-    this->tail->next = new_node;
-    this->tail = new_node;
+    this->tail_->next_ = new_node;
+    this->tail_ = new_node;
 }
 
 void LinkedList::Pop()
 {
-    if(this->head != this->tail)
+    if(this->head_ != this->tail_)
     {
-        Node* p1 = this->head;
-        Node* p2 = this->head->next;
+        Node* p1 = this->head_;
+        Node* p2 = this->head_->next_;
 
-        while(p2->next)
+        while(p2->next_)
         {
-            p1 = p1->next;
-            p2 = p2->next;
+            p1 = p1->next_;
+            p2 = p2->next_;
         }
 
         delete(p2);
-        p1->next = NULL;
-        this->tail = p1;
+        p1->next_ = NULL;
+        this->tail_ = p1;
     }
     else
     {
@@ -80,58 +80,58 @@ void LinkedList::Pop()
 }
 void LinkedList::Delete(string data)
 {
-    Node* prev = this->head;
-    Node* p = prev->next;
+    Node* prev = this->head_;
+    Node* p = prev->next_;
     bool found = false;
-    if(p->next)
+    if(p->next_)
     {
-        Node* next = p->next;
+        Node* next = p->next_;
         while(next && !found)
         {
-            if(p->data.compare(data) == 0)
+            if(p->data_.compare(data) == 0)
             {
-                prev->next = next;
+                prev->next_ = next;
                 if(next)
                 {
-                    next = next->next;
+                    next = next->next_;
                 }
                 else
                 {
                     next = NULL;
-                    this->tail = prev;
+                    this->tail_ = prev;
                 }
 
                 found = true;
             }
             if(!found)
             {
-                prev = prev->next;
-                p = p->next;
-                next = next->next;
+                prev = prev->next_;
+                p = p->next_;
+                next = next->next_;
             }
         }
     }
-    if(!found && this->tail->data == data)
+    if(!found && this->tail_->data_ == data)
     {
-        this->tail = prev;
-        prev->next = NULL;
+        this->tail_ = prev;
+        prev->next_ = NULL;
     }
 }
 void LinkedList::EmptyList()
 {
-    while(this->head != this->tail)
+    while(this->head_ != this->tail_)
     {
         Pop();
     }
 }
 void LinkedList::Display()
 {
-    Node* p = this->head;
+    Node* p = this->head_;
 
     while(p)
     {
-        cout << p->data << "->";
-        p = p->next;
+        cout << p->data_ << "->";
+        p = p->next_;
     }
     cout << endl;
 }
